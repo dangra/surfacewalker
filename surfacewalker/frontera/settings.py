08929e26d8213ba96e58ec4a97a46cca6f428867
Daniel Graña <dangra@gmail.com>
2016-10-31 16:38:39 -0300

    try to set consumer slot from jobid

--- surfacewalker/frontera/settings.py
+++ surfacewalker/frontera/settings.py
@@ -3,3 +3,7 @@ BACKEND = 'hcf_backend.HCFBackend'
 HCF_PROJECT_ID = os.getenv('SCRAPY_PROJECT_ID')
 HCF_FRONTIER = 'surfacewalker'
 #HCF_AUTH = 'override-in-local-settings'
+
+JOBID = os.getenv('SCRAPY_JOB')
+if JOBID:
+    HCF_CONSUMER_SLOT = int(JOBID.split('/')[-1]) % 8
