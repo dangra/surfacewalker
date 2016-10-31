949d79d61cdb4661a6421b8299e064108af07bb1
Daniel Graña <dangra@gmail.com>
2016-10-31 16:07:05 -0300

    disable HCF_AUTH setting

--- surfacewalker/frontera/settings.py
+++ surfacewalker/frontera/settings.py
@@ -2,4 +2,4 @@ import os
 BACKEND = 'hcf_backend.HCFBackend'
 HCF_PROJECT_ID = os.getenv('SCRAPY_PROJECT_ID')
 HCF_FRONTIER = 'surfacewalker'
-HCF_AUTH = 'override-in-local-settings'
+#HCF_AUTH = 'override-in-local-settings'
