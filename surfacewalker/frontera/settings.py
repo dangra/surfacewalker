70cd1de878bd1a56d5c60fb15b53f0131e483a7f
Daniel Graña <dangra@gmail.com>
2016-10-31 16:39:35 -0300

    fix projectid

--- surfacewalker/frontera/settings.py
+++ surfacewalker/frontera/settings.py
@@ -1,6 +1,6 @@
 import os
 BACKEND = 'hcf_backend.HCFBackend'
-HCF_PROJECT_ID = os.getenv('SCRAPY_PROJECT_ID')
+HCF_PROJECT_ID = int(os.getenv('SCRAPY_PROJECT_ID'))
 HCF_FRONTIER = 'surfacewalker'
 #HCF_AUTH = 'override-in-local-settings'
 
